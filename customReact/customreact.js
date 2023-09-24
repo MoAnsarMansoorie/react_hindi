c528088ac5a9376c707dd2c091793a6ae52e51f3
MoAnsarMansoorie <moansarmansoorie@gmail.com>
2023-09-24 22:21:38 +0530

    custom

--- customReact/customreact.js
+++ customReact/customreact.js
@@ -1,1 +1,33 @@
+function customReander(renderElement, container){
+    /*
+    const domElement = document.createElement(renderElement.type)
+    domElement.innerHTML = renderElement.children
+    domElement.setAttribute("href", renderElement.props.href)
+    domElement.setAttribute("target", renderElement.props.target)
+    
+    container.appendChild(domElement)
+    */
+
+    const domElement = document.createElement(renderElement.type)
+    domElement.innerHTML = renderElement.children
+    for (const prop in renderElement.props) {
+       if(prop === "children") continue;
+       domElement.setAttribute(prop, renderElement.props[prop])
+    }
+
+    container.appendChild(domElement)
+}
+
+
+const renderElement = {
+    type : "a",
+    props : {
+        href : "https://google.com",
+        target : "_blank"
+    },
+    children : "Click me to visit to google"
+}
+
 const mainContainer = document.querySelector("#root")
+
+customReander(renderElement, mainContainer)
